29b2d3825479d1e289222057f5c6f1a9bf783890
Aleksi Toivonen <aptoivonen@mail.com>
2021-06-07 15:53:55 +0300

    Introduce webpack

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgeCssPlugin = require("purgecss-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
+const CHROME_IN_WINDOWS = "chrome";
+
+module.exports = {
+  mode: isProduction ? "production" : "development",
+
+  entry: {
+    app: "./src/app.js",
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new PurgeCssPlugin({
+      paths: glob.sync(`${PATHS.src}/*.html`, { nodir: true }),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  devtool: isProduction ? "source-map" : "inline-source-map",
+
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    open: {
+      app: [CHROME_IN_WINDOWS, "--incognito"],
+    },
+  },
+};
