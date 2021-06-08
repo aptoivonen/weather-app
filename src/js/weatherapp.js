cebef1e48d33fb450181eb2558a6c5b8210cf8d8
Aleksi Toivonen <aptoivonen@mail.com>
2021-06-08 18:21:36 +0300

    Show weather data on screen

--- src/js/weatherapp.js
+++ src/js/weatherapp.js
@@ -6,7 +6,13 @@ class WeatherApp {
   constructor() {
     this.$form = document.forms["city-name-form"];
     this.$unitButton = document.getElementById("unit-button");
+    this.$city = document.getElementById("info-city");
+    this.$icon = document.getElementById("info-icon");
+    this.$title = document.getElementById("info-title");
+    this.$temperature = document.getElementById("info-temperature");
+    this.$humidity = document.getElementById("info-humidity-value");
     this.units = DEFAULT_UNIT;
+    this.weatherData = {};
     this._bind();
   }
 
@@ -18,27 +24,49 @@ class WeatherApp {
     );
   }
 
-  _handleWeather(event) {
-    event.preventDefault();
+  async _handleWeather(event) {
+    if (event) {
+      event.preventDefault();
+    }
     const cityName = this.$form.elements["city-name-input"].value;
     if (!cityName.trim()) {
       return;
     }
-    getWeather(cityName, this.units)
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
-
-    this.render();
+    try {
+      this.weatherData = await getWeather(cityName, this.units);
+      this.render();
+    } catch (err) {
+      this.weatherData = {};
+      console.log(err);
+    }
   }
 
   _handleUnitChange() {
     this.units = this.units === "celsius" ? "fahrenheit" : "celsius";
-    this.render();
+    this._handleWeather();
+  }
+
+  _getUnitPostfix() {
+    return this.units === "celsius" ? "°C" : "°F";
+  }
+
+  _getTemperaturePrefix(temp) {
+    return temp < 0 ? "-" : "+";
   }
 
   render() {
     this.$unitButton.classList.remove("celsius", "fahrenheit");
     this.$unitButton.classList.add(this.units);
+    const { name, country, temp, humidity, title, description, iconUrl } =
+      this.weatherData;
+    this.$city.textContent = `${name}, ${country}`;
+    this.$icon.src = iconUrl;
+    this.$icon.alt = description;
+    this.$title.textContent = title;
+    const tempPrefix = this._getTemperaturePrefix(temp);
+    const tempPostfix = this._getUnitPostfix();
+    this.$temperature.textContent = `${tempPrefix}${temp} ${tempPostfix}`;
+    this.$humidity.textContent = `${humidity} %`;
   }
 }
 
